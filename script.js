2ec8d00dcb2a60492c44c00262289b89b73a540d
Aswin Krishna <aswinkrishna2923@gmail.com>
2025-10-26 18:10:22 +0530

    Added webpage title

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ const decreaseBtn = document.getElementById('decreaseBtn');
 const increaseBtn = document.getElementById('increaseBtn');
 const resetBtn = document.getElementById('resetBtn');
 const countLabel = document.getElementById('countLabel');
-
 let count = 0;
-
 
 increaseBtn.onclick = function(){
     count++;
